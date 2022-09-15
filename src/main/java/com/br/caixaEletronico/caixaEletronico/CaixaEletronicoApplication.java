fbf72919ae46f47461ebfdc02cfec912af921d55
BrunoElias <bruno-150@hotmail.com>
2022-09-15 14:53:47 -0300

    Configuração banco

--- src/main/java/com/br/caixaEletronico/caixaEletronico/CaixaEletronicoApplication.java
+++ src/main/java/com/br/caixaEletronico/caixaEletronico/CaixaEletronicoApplication.java
@@ -2,7 +2,9 @@ package com.br.caixaEletronico.caixaEletronico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = "com.br.caixaEletronico.caixaEletronico.domain")
 @SpringBootApplication
 public class CaixaEletronicoApplication {
 
