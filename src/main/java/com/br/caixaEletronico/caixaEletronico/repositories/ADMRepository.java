ac93cafb15805a581c34a6e2fc68ff7175a32356
BrunoElias <bruno-150@hotmail.com>
2022-09-21 10:43:58 -0300

    Configuração de segurança

--- src/main/java/com/br/caixaEletronico/caixaEletronico/repositories/ADMRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.caixaEletronico.caixaEletronico.repositories;
-
-import com.br.caixaEletronico.caixaEletronico.domain.ADM;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ADMRepository extends JpaRepository<ADM, Long> {
-}
